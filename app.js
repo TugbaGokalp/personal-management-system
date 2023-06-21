2ade02f6dac95529f856bcd30864fef3600cdb76
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-06-21 14:08:57 +0300

    database and mongoose  created and added

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import dotenv from "dotenv";
+import conn from "./db.js" // db.js olmak zorunda yoksa ecmaScript calismiyor.
+
+dotenv.config();
+
+//connection to the database
+conn();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set('view engine', 'ejs');
 
@@ -10,6 +17,10 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('index')
 });
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Application running on port: ${port}`);
